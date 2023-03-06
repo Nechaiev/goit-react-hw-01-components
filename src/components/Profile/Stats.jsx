fe1da9038f9a026aac8b9fa4f3b10550d765135d
Stanislav <stanislav.nechaev@gmail.com>
2023-03-06 19:55:43 +0200

    add props

--- src/components/Profile/Stats.jsx
+++ src/components/Profile/Stats.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import s from './Profile.module.css';
 
@@ -21,3 +22,9 @@ const Stats = ({ followers, views, likes }) => {
   );
 };
 export default Stats;
+
+Stats.propTypes = {
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
